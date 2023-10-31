19669de4e0540f76f2e1c094b9ddc251d56d1047
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-10-31 10:35:17 +0530

    add postThread

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,7 @@
+function PostThread({userId}:{userId:string}){
+    return (<>
+    <h1 className="text-light-1">Post Thread Form</h1>
+    </>)
+}
+
+export default PostThread
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -20,7 +20,7 @@ export async function updateUser({
     bio,
     image,
     path,
-}: Params):Promise<void> {
+}: Params): Promise<void> {
     connectToDB();
 
     try {
@@ -42,6 +42,23 @@ export async function updateUser({
     }
     catch (error: any) {
         console.log(`Failed to create/update user: ${error.message}`)
+    }
+}
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User
+            .findOne({ id: userId })
+        // .populate({
+        //     path : 'communities',
+        //     model : 'Community'
+        // })
+
+    }
+    catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`)
     }
 }
 
