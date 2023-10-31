df1b8e51d363f769965a5f33a877078fc9eef9d5
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-10-31 11:35:10 +0530

    add Thread Form

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,7 +1,82 @@
-function PostThread({userId}:{userId:string}){
-    return (<>
-    <h1 className="text-light-1">Post Thread Form</h1>
-    </>)
+'use client'
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea"
+import { zodResolver } from '@hookform/resolvers/zod'
+// import Image from "next/image"
+// import { ChangeEvent, useState } from "react"
+// import { isBase64Image } from "@/lib/utils"
+// import { useUploadThing } from "@/lib/uploadThing"
+// import { updateUser } from "@/lib/actions/user.actions"
+import { threadValidation } from "@/lib/validations/thread";
+import { usePathname, useRouter } from "next/navigation"
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string,
+        bio: string,
+        image: string
+    };
+    btnTitle: string;
+}
+
+function PostThread({ userId }: { userId: string }) {
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const form = useForm({
+        resolver: zodResolver(threadValidation),
+        defaultValues: {
+            thread: '',
+            accountId: userId,
+        }
+    })
+
+    const onSubmit = () => {
+
+    }
+
+    return (
+        <>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="mt-10 flex- flex-col justify-start gap-10">
+                    <FormField
+                        control={form.control}
+                        name="thread"
+                        render={({ field }) => (
+                            <FormItem className="flex items-center gap-3 w-full">
+                                <FormLabel className="text-base-semibold text-light-2">
+                                    Content
+                                </FormLabel>
+                                <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                                    <Textarea
+                                        rows={15}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="bg-primary-500">Post Thread</Button>
+                </form>
+            </Form>
+        </>
+    );
+
 }
 
 export default PostThread
